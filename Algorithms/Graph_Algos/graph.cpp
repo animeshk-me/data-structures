a486a5675dd886510bd493f35b0814f9e1738de5
Animesh Kumar <animeshk@pop-os.localdomain>
2021-08-02 22:17:51 +0530

    Add UnDirected graph and functions

--- Algorithms/Graph_Algos/graph.cpp
+++ Algorithms/Graph_Algos/graph.cpp
@@ -38,11 +38,20 @@ class DirectedGraph {
   void print();
 };
 
+// Represents a Single UnDirectedGraph
+class UnDirectedGraph {
+  public:
+    int num_edges;
+    vector<Block*> vertices;
+  UnDirectedGraph() {
+      num_edges = 0; 
+  }
+  void add_edge(int vertex_id1, int vertex_id2);    // vertex1 --> vertex2
+  void print();
+};
 
 int main() {
     DirectedGraph G;
-    // Node * ptr = new Node(3);
-    // cout << ptr->id << endl;
     G.add_edge(1, 2);
     G.add_edge(2, 3);
     G.add_edge(2, 4);
@@ -58,7 +67,24 @@ int main() {
     G.add_edge(11, 12);
     G.add_edge(10, 9);
     G.add_edge(8, 9);
+    cout << "Number of edges in G: " << G.num_edges << endl;
     G.print();
+
+    UnDirectedGraph G2;
+    G2.add_edge(1, 2);
+    G2.add_edge(2, 3);
+    G2.add_edge(3, 5);
+    G2.add_edge(1, 4);
+    G2.add_edge(4, 5);
+    G2.add_edge(7, 4);
+    G2.add_edge(7, 6);
+    G2.add_edge(6, 11);
+    G2.add_edge(11, 10);
+    G2.add_edge(8, 10);
+    G2.add_edge(8, 9);
+    G2.add_edge(8, 7);
+    cout << "Number of edges in G2: " << G2.num_edges << endl;
+    G2.print();
     return 0;
 }
 
@@ -97,6 +123,23 @@ void DirectedGraph::add_edge(int vertex_id1, int vertex_id2) {
 
 
 void DirectedGraph::print() {
+    for(int i = 0; i < vertices.size(); i++) {
+        cout << "List[" << i << "]:";
+        vertices[i]->print();
+    }
+}
+
+void UnDirectedGraph::add_edge(int vertex_id1, int vertex_id2) {
+    int max_vertex_id = max(vertex_id1, vertex_id2);
+    for (int i = vertices.size(); i <= max_vertex_id; i++)
+        vertices.push_back(new Block());
+    vertices[vertex_id1]->append(vertex_id2);
+    vertices[vertex_id2]->append(vertex_id1);
+    num_edges += 1;
+}
+
+
+void UnDirectedGraph::print() {
     for(int i = 0; i < vertices.size(); i++) {
         cout << "List[" << i << "]:";
         vertices[i]->print();
