fb485c73fa26a43b48b12bdadc7f059855d9d38f
Animesh Kumar <animeshk@pop-os.localdomain>
2022-08-15 19:25:02 +0530

    Add Subarray sum

---
+++ tested_implementations/maximum_subarray_sum.cpp
@@ -0,0 +1,11 @@
+int maxSubArray(vector<int>& nums) {
+        int max_sum = INT_MIN;
+        int sum = 0;        
+        for(auto& a : nums) {
+            if(sum < 0)
+                sum = 0;
+            sum += a;
+            max_sum = max(max_sum, sum);
+        }
+        return max_sum;
+}
